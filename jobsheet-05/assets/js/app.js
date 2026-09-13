300fc209c69b48464a3315c0653ffe390ca6f397
Jonathan <>
2026-09-13 20:07:27 +0700

    changed the filter from no filter to judul filter

--- jobsheet-05/assets/js/app.js
+++ jobsheet-05/assets/js/app.js
@@ -33,7 +33,7 @@ function initTableFilter() {
         const keyword = input.value.toLowerCase();
         const rows = table.querySelectorAll("tbody tr");
         rows.forEach(function (row) {
-            const teks = row.textContent.toLowerCase();
+            const teks = row.querySelector("td").textContent.toLowerCase(); // ubah menjadi cari judul saja
             row.style.display = teks.includes(keyword) ? "" : "none";
         });
     });
