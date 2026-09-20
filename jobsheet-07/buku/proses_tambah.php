8352f91f96144257b682c48553fdba62ab91da53
Jonathan <>
2026-09-20 19:41:47 +0700

    added isbn validation

--- jobsheet-07/buku/proses_tambah.php
+++ jobsheet-07/buku/proses_tambah.php
@@ -24,6 +24,10 @@ if (!is_numeric($stok) || $stok < 0) {
     $errors[] = "Stok tidak boleh negatif.";
 }
 
+if ($isbn !== '' && !preg_match('/^[0-9-]+$/', $isbn)) {
+    $errors[] = "ISBN hanya boleh berisi angka dan tanda hubung.";
+}
+
 if (!empty($errors)) {
     $_SESSION['flash'] = ['type' => 'error', 'pesan' => implode(' ', $errors)];
     header('Location: tambah.php');
