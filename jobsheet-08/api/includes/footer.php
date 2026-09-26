2f8ad046f6eeb986faab96114870daae35078c83
Jonathan <>
2026-09-26 15:33:02 +0700

    modified the logo

--- jobsheet-08/api/includes/footer.php
+++ jobsheet-08/api/includes/footer.php
@@ -2,6 +2,7 @@
 
     <footer>
         <p><strong>VanguardArena</strong> &mdash; Forge Your Legacy, Dominate the Leaderboard.</p>
+        <p><strong>Desain dan Pemrograman Web</strong> &mdash; Jobsheet 08</p>
     </footer>
     <script src="<?php echo $base; ?>assets/js/app.js"></script>
 </body>
